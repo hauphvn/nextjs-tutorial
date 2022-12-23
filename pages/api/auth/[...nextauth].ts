b328116430236a8c8dd24e95bfcf308997c4e4d6
hauphvn <pvhau@cmcglobal.vn>
2022-12-23 10:48:42 +0700

    done course tutorial nextjs

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { providers } from "next-auth/core/routes";
 import * as process from "process";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "lib/mongodb";
 
 export default NextAuth({
   providers: [
@@ -10,15 +11,35 @@ export default NextAuth({
       clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`
     })
   ],
+  adapter: MongoDBAdapter(clientPromise),
   secret: `${process.env.NEXTAUTH_SECRET}`,
   callbacks: {
     // async redirect({ url, baseUrl }) {
     //   return url
     // },
-    async redirect({url, baseUrl}) {
-      if(url.startsWith('/')) return `${baseUrl}${url}`
-      else if(new URL(url).origin === baseUrl) return url
+    // async jwt({
+    //             token, user
+    //           }) {
+    //   if (user) {
+    //     token.id = user.id
+    //   }
+    //   return token;
+    // },
+    // async session({ session, token }) {
+    //   if (session.user && token?.id) {
+    //     const newUser = {
+    //       ...session.user,
+    //       id: token.id
+    //     }
+    //     session.user = {...newUser};
+    //   }
+    //   return session;
+    // },
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith('/')) return `${baseUrl}${url}`
+      else if (new URL(url).origin === baseUrl) return url
       return baseUrl;
     }
-  }
+  },
+
 })
